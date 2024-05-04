ce08fdeba348a6bdc9719467ba567bdfb11f967f
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-05-05 00:21:14 +0100

    created the data.jsx file to store data

---
+++ src/consts/data.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import nnamdi from '../assets/images/nnamdi.jpg'
+import edet from '../assets/images/edet.jpg'
+import grace from '../assets/images/grace.jpg'
+
+export const Testimony = [
+    {
+        id: 'nnamdi',
+        name: "God'sfavor Nnamdi",
+        image: nnamdi,
+        message: "Ma'am ❣️❣️❣️! I am where I am today al because of the knowledge you impacted in me. You inspire me so much to do better. Everyday I go to your page or status to see the wonders of your hands, God bless you so much. You're my role model and my mentor, I owe you so much. I just lack words to express myself and all you've added to my life. Ma, I pray for you everyday and God will continue to increase you. I love you so much mama❣️❣️❤️❤️🥰🥰"
+    },
+    {
+        id: 'edet',
+        name: "Elizabeth Edet Eyoh",
+        image: edet,
+        message: "My coming all the way from Uyo to train with you was not in vain, I was having order here and there, you are good at what you do, keep the good work going. I don't have silver nor gold to give you at this time just to say thank you for all you did for me when I was in Sovise Cook&Bake Academy, but I know with time I will start spraying you plenty money💰💰 heart of gratitude🙏🙏🙏💯💯💯"
+    },
+    {
+        id: 'grace',
+        name: "Ibulubo Grace Tamuno-idikibugerere",
+        image: grace,
+        message: "Thanks for everything mother chef at the time I was about giving up you held my hand and lifted me up you're really a rare gem ma. You made me believe in myself that I can do it and by God's grace I made it, thank you very much ma. I don't know how to thank you ma but God will reward you."
+    }
+]
