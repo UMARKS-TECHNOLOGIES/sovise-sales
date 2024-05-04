826fcabdb3b68a6881e6567f88119ea524ed14b1
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-05-05 00:22:40 +0100

    created the Home.jsx file inside the pages folder to serve as the index page after loading of the site

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import OurTeam from '../components/OurTeam'
+import Testimonials from '../components/Testimonials'
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <Hero />
+        <OurTeam />
+        <Testimonials />
+    </div>
+  )
+}
+
+export default Home
