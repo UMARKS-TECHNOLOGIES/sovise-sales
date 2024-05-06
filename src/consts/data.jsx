4c79d8564d4d76f4d55ed576b035d683aec44c5c
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-05-06 14:10:17 +0100

    added the constants for the different class plans

--- src/consts/data.jsx
+++ src/consts/data.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 import nnamdi from '../assets/images/nnamdi.jpg'
 import edet from '../assets/images/edet.jpg'
 import grace from '../assets/images/grace.jpg'
+import cake1 from '../assets/images/cake1.png'
+import cake2 from '../assets/images/cake2.png'
+import group1 from '../assets/images/group1.png'
+import group2 from '../assets/images/group2.png'
+import group3 from '../assets/images/group3.png'
+import group4 from '../assets/images/group4.png'
+import castle from '../assets/images/castle.png'
+import cultural from '../assets/images/cultural.png'
+import group5 from '../assets/images/group5.png'
+import single from '../assets/images/single.png'
+import three from '../assets/images/three.png'
+import two from '../assets/images/two.png'
+import group6 from '../assets/images/group6.png'
+import general from '../assets/images/general.png'
+import proChef from '../assets/images/pro-chef.png'
+import singleChef from '../assets/images/single-chef.png'
 
 export const Testimony = [
     {
@@ -22,4 +38,169 @@ export const Testimony = [
         image: grace,
         message: "Thanks for everything mother chef at the time I was about giving up you held my hand and lifted me up you're really a rare gem ma. You made me believe in myself that I can do it and by God's grace I made it, thank you very much ma. I don't know how to thank you ma but God will reward you."
     }
+]
+
+export const BakingClasses = [
+    {
+        id: 'no-decoration',
+        title: 'CAKE BAKING CLASS (NO DECORATION)',
+        img: cake1,
+        price: '#53,750 (FEE)',
+        days: `5 days`,
+        withAccomodation: '#60,000 with Accomodation',
+        outline: `Learn how to make 6 different types of cakes, Learn these cakes in different ways of making them, Learn yummy fluffy cakes, Learn cake filling, Learn cake slices, Learn cake packaging, Learn how to sell your cakes, Learn how to get more customers`,
+        notice: `Students provide for their cake practicals. Class days (Monday to Friday). Class time (10am to 2pm)`,
+        registration: 'Registration is 5k and covers (Registration fees, Apron, Book and Certificate)',
+        passport: `1 Passport for Registration`
+    },
+    {
+        id: 'beginner-baking-class',
+        title: 'BEGINNER BAKING CLASS',
+        img: cake2,
+        price: '#60,000 (FEE)',
+        weeks: `4 weeks`,
+        withAccomodation: '#90,000 with Accomodation',
+        outline: `Learn how to bake 8 different types of cakes, How to cover cake with perfect Foundant icing for all weather, Buttercream with chocolate ganache, How to make character cakes, How to make ombre buttercream, Learn the cake business, How to use social media to sell your products, Photography, Costing`,
+        notice: `You will learn food photography, the art of cake business and selling strategies. Students provide for their cake practicals. Class days (Monday, Wednesday and Friday). Class time (10am to 3pm)`,
+        registration: 'Registration is 10k and covers (Registration fees, Apron, Book and Certificate)',
+        passport: `1 Passport for Registration`
+    },
+    {
+        id: 'beginner-intermediate-class',
+        title: 'BEGINNER/INTERMEDIATE BAKING CLASS',
+        img: group1,
+        price: '#86,000 (FEE)',
+        weeks: `6 weeks`,
+        withAccomodation: '#146,000 with Accomodation',
+        outline: `Learning how to Bake 8 different types of cakes, Foundant covering with neat, smooth sharp edges, Buttercream covering with chocolate ganache, Whipped cream covering, How to use molds, How to use stencil, Trimming and leveling of cake, How to convery the cakes to the event venue, Properly use of molds, Merchandising and lots more`,
+        notice: `You will learn food photography, the art of cake business and selling strategies. Students provide for their cake practicals. Class days (Monday, Wednesday and Friday). Class time (10am to 3pm)`,
+        registration: 'Registration is 10k and covers (Registration fees, Apron, Book and Certificate)',
+        passport: `1 Passport for Registration`
+    },
+    {
+        id: 'advanced-decorating-class',
+        title: 'ADVANCE DECORATING WEDDING BAKING CLASS',
+        img: group4,
+        price: '#60,000 (FEE)',
+        weeks: `2 weeks`,
+        withAccomodation: '#90,000 with Accomodation',
+        outline: `Traditional Wedding Cake, Calabash, Beads, Etibo Shirt, White Wedding Cake, Learn perfect cake Trimming Round and Square, How to make perfect fondant for all weather, How to cover perfect covering with neat sharp edges, How to use stencil for all design, How to make realistic sugar flowers, How to do crumbs coating without spoiling your cake, How to stack cakes, How to transport to event venue, How to market your products and many more`,
+        notice: `Students provide for their cake practicals. Class days (Monday, Wednesday and Friday). Class time (10am to 4pm)`,
+        registration: 'Registration is 10k and covers (Registration fees, Apron, Book and Certificate)',
+        passport: `1 Passport for Registration`
+    },
+    {
+        id: 'beginner-advance-class',
+        title: 'BEGINNER TO ADVANCE DECORATING WEDDING CLASS',
+        img: castle,
+        price: '#126,000 (FEE)',
+        months: `2 months`,
+        withAccomodation: '#186,000 with Accomodation',
+        outline: `Learning how to bake 8 different types of cakes, Fondant covering with neat, smooth sharp edges, Buttercream covering with chocolate ganache, Traditional wedding cake, calabash, beads, etibo shirt, White wedding cake, How to make edible Sugar flowers, How to use molds, How to use stencil, Trimming and leveling of cake, How to dowel and stack cakes, How to convey the cake to the event venue, Proper use of molds, Photography, Costing, Merchandising and lots more`,
+        notice: `Students provide for their cake practicals. Class days (Monday, Wednesday and Friday). Class time (10am to 3pm)`,
+        registration: 'Registration is 10k and covers (Registration fees, Apron, Book and Certificate)',
+        passport: `1 Passport for Registration`
+    },
+    {
+        id: 'snack-pastry-class',
+        title: 'SNACK AND PASTRY CLASS',
+        img: cultural,
+        price: '#95,000 (FEE)',
+        weeks: `4 weeks`,
+        withAccomodation: '#85,000 with Accomodation',
+        outline: `Learning how to make: meat pie, bread, corn dog, milkshakes, burger, shawarma, sandwich, waffle, beef kebab, money bag, yummy butter cookies, sausage roll, cinnamon roll, egg roll, fluffy doughnuts, crunchy milky chin chin, fluffy puff puff, samosa, spring roll, pizza, chapman, understanding the business and lots more`,
+        notice: `You will learn food photography, the art of food business and selling strategies. Students provide for their cake practicals. Class days (Tuesday and Thursday). Class time (10am to 3pm)`,
+        registration: 'Registration is 10k and covers (Registration fees, Apron, Book and Certificate)',
+        passport: `1 Passport for Registration`
+    },
+    {
+        id: 'food-entrepreneurship-class',
+        title: 'FOOD ENTREPRENEURSHIP COOKERY CLASS',
+        img: group5,
+        price: '#250,000 (FEE)',
+        months: `2 months`,
+        withAccomodation: '#280,000 with Accomodation',
+        outline: `Learning the ethics of cooking, food planning, food photography, budgeting and grocery list, cooking techniques, events catering, food safety and hygiene, cooking (African and Continental meals), breakfast, lunch, dinner and many more`,
+        notice: `You will learn food photography, the art of food business and selling strategies. Students provide practical ingredients by contributing among themselves. Class days (Tuesday and Thursday). Class time (10am to 3pm)`,
+        registration: 'Registration is 25k and covers (Registration fees, Apron, Book and Certificate)',
+        passport: `1 Passport for Registration`
+    },
+    {
+        id: 'cooking-baking-class',
+        title: 'COOKING AND BAKING CLASS',
+        img: single,
+        price: '#350,000 (FEE)',
+        months: `3 months`,
+        withAccomodation: '#440,000 with Accomodation',
+        outline: `Beginner to advance decorating wedding baking class, learning the ethics of cooking, food planning, food photography, budgeting and grocerylist, cooking techniques, events catering, food safety and hygiene, cooking (African and Continental meals), breakfast, lunch, dinner and many more`,
+        notice: `You will learn food photography, the art of food business and selling strategies. Students provide practical ingredients by contributing among themselves. Class days (Monday to Friday). Class time (10am to 3pm)`,
+        registration: 'Registration is 25k and covers (Registration fees, Apron, Book and Certificate)',
+        passport: `1 Passport for Registration`
+    },
+    {
+        id: 'desert-class',
+        title: 'DESERT CLASS',
+        img: three,
+        price: '#120,000 (FEE)',
+        weeks: `1 week`,
+        withAccomodation: '#150,000 with Accomodation',
+        outline: ``,
+        notice: `Class training ingredients are provided by the Academy except exams. Class days (Tuesday and Thursday). Class time (10am to 3pm)`,
+        registration: 'Registration is 10k and covers (Registration fees, Apron, Book and Certificate)',
+        passport: `1 Passport for Registration`
+    },
+    {
+        id: 'diploma-cuisine-class',
+        title: 'DIPLOMA IN CUISINE AND PATISSERIE CLASS',
+        img: two,
+        price: '#450,000 (FEE)',
+        months: `2 months`,
+        withAccomodation: '#510,000 with Accomodation',
+        outline: `Cooking (Africa and Continental Meals), understanding spices and herbs, dessert, snacks pastry, bread making, cocktails, smoothies, fresh juice, event cooking, event table settings, business branding, food costing, food photography, food styling, food presentation, how to use social media and business sales`,
+        notice: `Class training ingredients are provided by the Academy except exams. Class days (Monday, Wednesday and Friday). Class time (10am to 3pm)`,
+        registration: 'Registration is 25k and covers (Registration fees, Chef Jacket, Apron, Chef Cap, Book and Certificate)',
+        passport: `1 Passport for Registration`
+    },
+    {
+        id: 'professional-diploma-cuisine-class',
+        title: 'PROFESSIONAL CHEFS DIPLOMA IN CUISINE AND PATISSERIE',
+        img: group6,
+        price: '#750,000 (FEE)',
+        months: `5 months`,
+        withAccomodation: '#900,000 with Accomodation',
+        outline: `Baking (Beginner to advance), cooking (Africa and Continental meals), understanding spices and herbs, dessert, snacks pastry, bread making, cocktails, smoothies, fresh juice, event table settings, business branding, food costing, food photography, food styling and plating, food presentation, food safety/hygiene, how to use social media for business sales`,
+        notice: `Class training ingredients are provided by the Academy except exams. Class days (Monday to Friday). Class time (10am to 3pm)`,
+        registration: 'Registration is 25k and covers (Registration fees, Chef Jacket, Apron, Chef Cap, Book and Certificate)',
+        passport: `1 Passport for Registration`,
+    },
+    {
+        id: 'professional-kitchen-managemnt-class',
+        title: 'PROFESSIONAL CHEFS KITCHEN MANAGEMENT - COMBINATION CLASS',
+        breakdown: `PROFESSIONAL CHEFS DIPLOMA (3 MONTHS) MON - FRI, 10AM - 3PM. KITCHEN OPERATIONS INTERNSHIP (3 MONTHS) in a fine dining restaurant operated by Sovise Academy`,
+        img: general,
+        price: '#1,000,000 (FEE)',
+        months: `6 months`,
+        outline: `Cooking (Africa and Continental meals), understanding spices and herbs, dessert, snacks pastry, bread making, cocktails, smoothies, fresh juice, event cooking, event table settings, business branding, food costing, food photography, food styling, food presentation, kitchen management, gain mastery of advanced culinary techniques and kitchen management skills, menu planning and designing for balanced and profitable menus, inventory control and procurement to run efficient kitchen, cost management in the kitchen.`,
+        notice: `Class training ingredients are provided by the Academy except exams`,
+        registration: 'Students are given Chef Jacket, Apron, Chef Cap, Book and Certificate after graduation.'
+    },
+    {
+        id: 'professional-kitchen-managemnt-class-one-year',
+        title: 'PROFESSIONAL CHEFS KITCHEN MANAGEMENT - COMBINATION CLASS (1 YEAR)',
+        breakdown: `PROFESSIONAL CHEFS DIPLOMA (6 MONTHS) MON - FRI, 10AM - 3PM. KITCHEN OPERATIONS INTERNSHIP (6 MONTHS) in a fine dining restaurant operated by Sovise Academy`,
+        img: proChef,
+        price: '#2,000,000 (FEE)',
+        year: `1 year`,
+        outline: `Baking (Beginner to advance), cooking (Africa and Continental meals), understanding spices and herbs, dessert, snacks pastry, bread making, cocktails, smoothies, fresh juice, event cooking, event table settings, business branding, food costing, food photography, food styling, food presentation, kitchen management, gain mastery of advanced culinary techniques and kitchen management skills, menu planning and designing for balanced and profitable menus, inventory control and procurement to run efficient kitchen, cost management in the kitchen.`,
+        notice: `Class training ingredients are provided by the Academy except exams`,
+        registration: 'Students are given Chef Jacket, Apron, Chef Cap, Book and Certificate after graduation.'
+    },
+    {
+        id: 'private-class-with-chef-sophia',
+        title: 'THE CAFE EXPERIENCE - A PRIVATE CLASS WITH CHEF SOPHIA',
+        img: singleChef,
+        outline: `Upgrade class in culinary art`,
+        notice: `Class training ingredients are provided by the Academy except exams. Class day (Monday to Friday). Class time (10am to 3pm)`,
+        registration: 'Students are given Chef Jacket, Apron, Chef Cap, Book and Certificate after graduation.'
+    },
 ]
